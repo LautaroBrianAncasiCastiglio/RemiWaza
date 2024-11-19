b4a746b12c7f5ece7213c0063dc6a0e537b08e66
lautarocas05 <lautarocas05@gmail.com>
2024-11-19 20:27:57 +0000

    arreglo de auto

--- app/src/main/java/com/calipso/remiwaza/ParametrosEmpleados.kt
+++ app/src/main/java/com/calipso/remiwaza/ParametrosEmpleados.kt
@@ -5,9 +5,8 @@ data class ParametrosEmpleados(
     val name: String = "",
     val lastName: String = "",
     val state: String = "",
-    val email: String = "", // Si necesitas este campo
-    val type: String = "", // Si necesitas este campo
-    val password: String = "" // Si necesitas este campo
+    val modelo: String = "", // Si necesitas este campo
+    val marca: String = "", // Si necesitas este campo
 ) {
     // Constructor sin argumentos que Firebase necesita ya está implícito al usar valores predeterminados
 }
